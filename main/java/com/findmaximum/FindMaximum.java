6a377ed602fbf1fcb825ab960bc0be3656fadbc2
viresh rawool <vireshrawool007@gmail.com>
2021-02-27 15:19:49 +0530

    Added: method to calculate in geniric method

--- main/java/com/findmaximum/FindMaximum.java
+++ main/java/com/findmaximum/FindMaximum.java
@@ -2,18 +2,12 @@ package com.findmaximum;
 
 import java.util.*;
 
-public class FindMaximum {
-    public Comparable findIntMaximum(List myNumberList) {
-        Comparable max = Collections.max(myNumberList);
-        return max;
-    }
+public class FindMaximum<T>{
+    private T[] inputArray;
 
-    public Comparable findFloatMaximum(List myDoubleList) {
-        Comparable max = Collections.max(myDoubleList);
-        return max;
-    }
-    public static Comparable findStringMaximum(List myStringList) {
-        Comparable max = Collections.max(myStringList);
+    public <T>Comparable findGenericMaximum(T[] myNumber){
+        List myNumberList = Arrays.asList(myNumber);
+        Comparable max = Collections.max(myNumberList);
         return max;
     }
 }
--- test/java/com/findmaximum/FindMaximumTest.java
+++ test/java/com/findmaximum/FindMaximumTest.java
@@ -3,64 +3,68 @@ package com.findmaximum;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class FindMaximumTest {
 
     @Test
-    public void givenIntMaximumAtFirstPosition(){
+    public void givenIntMaximumAtFirstPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<Integer> arrayList = Arrays.asList(10, 4, 2);
-        Assert.assertEquals(10,checkMax.findIntMaximum(arrayList));
-    }
-    @Test
-    public void givenIntMaximumAtSecondPosition(){
-        FindMaximum checkMax = new FindMaximum();
-        List<Integer> arrayList = Arrays.asList(4,10,2);
-        Assert.assertEquals(10,checkMax.findIntMaximum(arrayList));
-    }
-    @Test
-    public void givenIntMaximumAtThirdPosition(){
-        FindMaximum checkMax = new FindMaximum();
-        List<Integer> arrayList = Arrays.asList(4, 2, 10);
-        Assert.assertEquals(10,checkMax.findIntMaximum(arrayList));
+        Object[] arr = {10, 2, 1,5,4};
+        Assert.assertEquals(arr[0], checkMax.findGenericMaximum(arr));
     }
 
     @Test
-    public void givenFloatMaximumAtFirstPosition(){
+    public void givenIntMaximumAtSecondPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<Double> arrayList = Arrays.asList(10.0, 4.0, 2.0);
-        Assert.assertEquals(10.0,checkMax.findFloatMaximum(arrayList));
+        Object[] arr = {2,10,1,3,7};
+        Assert.assertEquals(arr[1], checkMax.findGenericMaximum(arr));
     }
+
     @Test
-    public void givenFloatMaximumAtSecondPosition(){
+    public void givenIntMaximumAtThirdPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<Double> arrayList = Arrays.asList( 4.0,10.0, 2.0);
-        Assert.assertEquals(10.0,checkMax.findFloatMaximum(arrayList));
+        Object[] arr = {2,1,10};
+        Assert.assertEquals(arr[2], checkMax.findGenericMaximum(arr));
     }
+
     @Test
-    public void givenFloatMaximumAtThirdPosition(){
+    public void givenFloatMaximumAtFirstPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<Double> arrayList = Arrays.asList(2.0, 4.0,10.0);
-        Assert.assertEquals(10.0,checkMax.findFloatMaximum(arrayList));
+        Object[] arr = {10.0, 1.0, 2.0};
+        Assert.assertEquals(arr[0], checkMax.findGenericMaximum(arr));
     }
+
     @Test
-    public void givenStringMaximumAtFirstPosition(){
+    public void givenFloatMaximumAtSecondPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<String> stringList = Arrays.asList("Peach","Apple","Banana");
-        Assert.assertEquals("Peach",checkMax.findStringMaximum(stringList));
+        Object[] arr = {2.0, 10.0, 1.0};
+        Assert.assertEquals(arr[1], checkMax.findGenericMaximum(arr));
     }
+
     @Test
-    public void givenStringMaximumAtSecondPosition(){
+    public void givenFloatMaximumAtThirdPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<String> stringList = Arrays.asList("Apple","Peach","Banana");
-        Assert.assertEquals("Peach",checkMax.findStringMaximum(stringList));
+        Object[] arr = {2.0, 1.0, 10.0};
+        Assert.assertEquals(arr[2], checkMax.findGenericMaximum(arr));
     }
+
     @Test
-    public void givenStringMaximumAtThirdPosition(){
+    public void givenStringMaximumAtFirstPosition() {
         FindMaximum checkMax = new FindMaximum();
-        List<String> stringList = Arrays.asList("Apple","Banana","Peach");
-        Assert.assertEquals("Peach",checkMax.findStringMaximum(stringList));
+        Object[] arr = {"Peach", "Banana", "Apple"};
+        Assert.assertEquals(arr[0], checkMax.findGenericMaximum(arr));
+    }
+
+    @Test
+    public void givenStringMaximumAtSecondPosition() {
+        FindMaximum checkMax = new FindMaximum();
+        Object[] arr = {"Apple", "Peach", "Banana"};
+        Assert.assertEquals(arr[1], checkMax.findGenericMaximum(arr));
+    }
+
+    @Test
+    public void givenStringMaximumAtThirdPosition() {
+        FindMaximum checkMax = new FindMaximum();
+        Object[] arr = {"Banana", "Apple", "Peach"};
+        Assert.assertEquals(arr[2], checkMax.findGenericMaximum(arr));
     }
 }
